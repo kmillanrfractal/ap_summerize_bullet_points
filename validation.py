dd13d9bbcf6b0ec2d9f12906b5851f4c3c0e8f56
arturo1994 <arturo.carreno@wizeline.com>
2024-09-10 12:46:42 -0400

    update validations

--- validation.py
+++ validation.py
@@ -1,19 +1,25 @@
 from stuff_model import model
+from refine import model as refine
 from prompts import prompts
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.output_parsers import PydanticOutputParser
 from langchain.pydantic_v1 import BaseModel, Field
+import time
+import pandas as pd
 
 
 def summary_validation(
-    doc, summary_prompt=prompts.summary_prompt, model_base_name="gpt-4o-mini"
+    doc,
+    summary_prompt=prompts.basic_summary_prompt,
+    model_base_name="gpt-4o-mini",
 ):
+    start_time = time.time()
     llm = ChatOpenAI(temperature=0, model_name=model_base_name)
 
     base_response_output_key = "base_response"
-    base_response_chain = model.stuff_model(
+    base_response_chain = refine.refine_model(
         doc=doc, prompt=summary_prompt, model_name=model_base_name
     )
 
@@ -119,13 +125,27 @@ def summary_validation(
                 }
             )["answer"]
         )
-
-    return answer, verification_questions, claimed_facts
+    end_time = time.time()  # End timing
+    execution_time = end_time - start_time  # Calculate total execution time
+    resume = base_response_chain[0]
+    pd.DataFrame(
+        {
+            "Question": verification_questions,
+            "Answer": claimed_facts,
+            "Agent Review": answer,
+        }
+    ).to_csv(
+        "validations.csv",
+        index=False,
+    )
+    return answer, verification_questions, claimed_facts, execution_time
 
 
 if __name__ == "__main__":
     summary_validation(
-        doc="""MEXICO CITY (AP) — The smell of hairspray clouds Alexa Flores López as she gets the finishing touches of an intricate updo on her thick black hair. When Alexa found out she was going to have a quinceañera — the traditional celebration for a 15th birthday in Mexico — she could barely contain herself.\n\She got super excited, like her heart would come out!\" said her mother, Carmen López Díaz. \"She was just counting down the days.\"\nAt the Federico Gomez Children\\'s Hospital in Mexico City, volunteers clad in white and blue nurse uniforms scurry to attend to birthday girls and boys. They curl hair, do makeup, and adjust bow ties.\nIt\'s all for the hospital\'s annual \"Mis XV\" or \"My Fifteenth\" event. Whether the young patients are in treatment for a serious disease or have overcome cancer, the hospital celebrates the coming-of-age of these teenagers. After going through expensive treatments, some families can\'t afford to pay for a party — with the hospital providing them an alternative.\n\"We\'ve really just bought shoes because the hospital handles everything,\" said Díaz. \nThe hospital started throwing the event in 2017 after volunteer nurses caught wind that a beloved patient would turn 15 soon. They took it as an opportunity to organize a celebration for her and eventually turned it into an annual hospital event that\'s been going strong for seven years.\nIn Mexico, the \"quinceñera\" or \"fifteenth birthday\" is a huge rite of passage for adolescents, particularly girls. The occasion marks the transition of a teen into adulthood.\nThough Alexa\'s birthday was last month, the hospital allows patients to participate if their birthdays happen within the year of the celebration. \nShe has been a patient at the hospital the past three years receiving treatment for lupus. Her younger sister was just diagnosed with the same disease last year, making things more difficult for the family. However, the hospital\'s celebration has been something to look forward to. \n\"We never imagined so much — we thought this was going to be a regular hospital event,\" her mother said. \nFrancesca Solórzano, who has been a volunteer at the hospital for 17 years, assists Alexa on her big day and attends to her every need. Solórzano makes sure the teen has enough water and showers hairspray onto her dress to make sure its fabric remains crisp. \nShe also sports a silver and blue pin that matches Alexa\'s bouquet.\n\"I receive more than I give,\" Solórzano says about volunteering. \"I give my time here and I get a lot of blessings in return.\"\nSolórzano takes Alexa into a dressing area to change into her dress. When she comes out in a deep navy blue gown sprinkled with sequins, volunteers exclaim, \"How pretty!\" Another volunteer takes her cellphone out and snaps a picture. \"She looks like a princess!\" \nOn the other end of the room, Carlos Emilio Escalona García, 15, takes a seat with his mom after getting suited up. \nFor his mother, Marta Magdalena García Chávez, the day is filled with nothing but joy.\n\"It\'s really beautiful to see all of this happening,\" she said. \"He just had surgery so we wouldn\'t have had the possibility to have a party.\"\nCarlos has been a patient with the hospital for 13 years. He has dealt with heart problems since he was a child, and just underwent his fourth surgery. His mother said he gets tired often, but is still motivated to make the most out of every day.\n\"Like every other teenager, my favorite class is physical education,\" Carlos says. \"I also really enjoy doing math — I have so much fun doing different operations.\"\nThe preparations for the hospital\'s extravaganza have been a long time in the making. As early as January the hospital is already reaching out to make-up artists and other vendors to see if they want to participate. The entire event comes together free of charge.\nAfter getting fitted into their regal looks, it\'s time for the party to start.\nDownstairs in the banquet hall, family members crowd the dance floor, craning their necks to get a look at the teenagers. As they walk in with their partners, Carlos and Alexa end up lined up side by side.\n\"A round of applause for our fifteen-year-olds!\" says someone in the crowd.\nAs they kick off the first dance, Alexa takes the hand of her partner and her mom zooms in on her phone to get a video. After the chorus of the song settles, her mother takes a deep breath and puts her phone away — ready to capture the moment.\nShe stares in awe at her daughter as she twirls in her blue tulle skirt.\n""",
-        summary_prompt=prompts.summary_prompt,
-        model_base_name="gpt-4o-mini",
+        doc="""
+JERUSALEM (AP) — An Israeli ground offensive in the Gaza Strip would further escalate the war raging since Hamas launched its unprecedented attack, killing hundreds of civilians. A ground invasion also would threaten to draw in the Iranian-backed Lebanese militant group Hezbollah from the north. The United States has deployed one aircraft carrier group to the region, with another on the way later this week — reflecting concerns of a widening conflict and meant to establish a force that deters Iran and others.   It would be the the third major ground assault in Gaza since Israel left the seaside enclave in 2005.   Here's what is known about the weapons of those who would be involved.   ISRAEL  Israel's military has long been supported by the United States, with $3.3 billion in congressionally mandated annual funding, plus another $500 million toward missile defense technology.  Israel is one of the best-armed nations in the wider Middle East. Its air force includes the advanced American F-35 fighter jet, missile defense batteries including the American-made Patriot, and the Iron Dome missile defense system.  Israel has armored personnel carriers and tanks, and a fleet of drones and other technology available to support any street-to-street battles. Israel trains soldiers on such techniques at its Urban Warfare Training Center, known colloquially as “Mini Gaza.”  Israel has some 170,000 troops typically on active duty and has called up some 360,000 reservists for the war — three-fourths of its estimated capacity, according to the International Institute for Strategic Studies.   Israel has also long maintained an undeclared nuclear weapons program.   HAMAS    Hamas, the rulers of the Gaza Strip since 2007, do not have the billions of dollars in aid and advanced weaponry of the Israeli military. But the surprise weekend attack included militants on paragliders and grenade-dropping drones, and Hamas can leverage guerrilla warfare tactics that could make any ground assault dangerous for Israeli troops.   Hamas has 15,000 to 20,000 fighters, the International Institute for Strategic Studies estimates. Israel puts the number higher, at up to 30,000 fighters.   The Hamas arsenal includes assault rifles, heavy machine guns, rocket-propelled grenades and anti-tank weapons, as well as longer-range sniper rifles. In the past, Hamas has employed boobytraps and suicide bombers.   Though Israel has a vast missile defense network, Hamas has created a vast supply of locally manufactured missiles with the aim of firing multiple salvos to break through. The Israeli military says over 5,000 missiles have been fired toward the country since the war began. Israeli intelligence in 2021 estimated Hamas and Islamic Jihad, another militant group operating in Gaza, had some 30,000 missiles in their arsenal. Analysts say there are no signs, yet, that Hamas has developed guided missiles, which can more precisely strike at targets.   HEZBOLLAH  The Iranian-backed Lebanese militant group Hezbollah sits just across Israel's northern border. Since Hamas’ attack, there have been limited exchanges of fire between Hezbollah and Israel, but no wide-scale offensive. But the forces Hezbollah could bring remain a concern for Israel.   Hezbollah's leader, Hassan Nasrallah, had boasted that the group has 100,000 fighters, though other estimates put its troop strength at less than half that.   Hezbollah holds a vast arsenal “mostly of small, man-portable and unguided surface-to-surface artillery rockets,” according to the Center for Strategic and International Studies. The U.S. has estimated Hezbollah and other militant groups in Lebanon have some 150,000 missiles and rockets. Hezbollah also has been working on precision-guided missiles.   Hezbollah has previously launched drones into Israel. Its forces also have assault rifles, heavy machine guns, rocket-propelled grenades, roadside bombs and other weaponry.   THE UNITED STATES   The U.S. military has sent the aircraft carrier USS Gerald R. Ford and its battle group to the eastern Mediterranean to provide air support if needed to Israel with its surveillance aircraft and F-18 fighter jets.   Meanwhile, the USS Dwight D. Eisenhower carrier strike group will leave its homeport of Norfolk, Virginia, and sail for the Mediterranean, potentially doubling the Navy’s Israel response.   The U.S. military also maintains a vast network of bases across the wider Middle East. However, because Israel's punishing airstrike campaign on the Gaza Strip has inflamed anger among Muslims in the wider region, the U.S. will likely be asked not to fly any potential air missions out of those nations. That would force the Pentagon to rely on carrier-based launches to provide any support to Israel it may need.
+""",
+        summary_prompt=prompts.summary_prompt_without_zero_shot,
+        model_base_name="gpt-4o",
     )
